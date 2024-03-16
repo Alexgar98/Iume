581e584f5e13e534f52085836a46923207200d61
Alex <92014559+Alexgar98@users.noreply.github.com>
2024-03-16 22:27:46 +0100

    Cup and exports
    Single elimination done. Also some tests to export classifications to a CSV file and a result log to a text file

--- src/main/java/classes/Cup.java
+++ src/main/java/classes/Cup.java
@@ -2,6 +2,7 @@ package classes;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Cup {
 	
@@ -28,15 +29,95 @@ public class Cup {
 	public void generateRound() {
 		int nTeams = teams.size();
 		ArrayList<Team> used = new ArrayList<Team>();
+		Random rd = new Random();
+		Team bye = new Team("Bye", (byte) 0);
 		
 		//In case the number of teams is odd
 		if (nTeams % 2 != 0) {
-			teams.add(new Team("Bye", (byte) 0));
+			teams.add(bye);
 			nTeams++;
 		}
 		
 		Collections.shuffle(teams);
 		
+		for (int i = 0; i < nTeams/2; i++) {
+			Team home;
+			Team away;
+			
+			do {
+				home = teams.get(rd.nextInt(nTeams));
+			} while (used.contains(home));
+			used.add(home);
+			
+			do {
+				away = teams.get(rd.nextInt(nTeams));
+			} while (used.contains(away));
+			used.add(away);
+			
+			if (!home.equals(bye) && !away.equals(bye)) {
+				matches.add(new Match(home, away));
+			}
+		}
+		teams.remove(bye);
+	}
+	
+	//Penalty shootout. Works with at least a 5d4 for each team; 1 fails the penalty, other numbers score
+	public Team simulatePenalties(Match drawn) {
+		Random rd = new Random();
+		byte pHome = 0;
+		byte pAway = 0;
+		byte dice;
+		
+		//5 first shoots
+		for (byte i = 0; i < 5; i++) {
+			dice = (byte) (rd.nextInt(4) + 1);
+			if (dice != 1) {
+				pHome++;
+			}
+			dice = (byte) (rd.nextInt(4) + 1);
+			if (dice != 1) {
+				pAway++;
+			}
+		}
+		
+		//Sudden death
+		while (pHome == pAway) {
+			dice = (byte) (rd.nextInt(4) + 1);
+			if (dice != 1) {
+				pHome++;
+			}
+			dice = (byte) (rd.nextInt(4) + 1);
+			if (dice != 1) {
+				pAway++;
+			}
+		}
+		
+		Team loser;
+		if (pHome > pAway) {
+			loser = drawn.getAway();
+		}
+		else {
+			loser = drawn.getHome();
+		}
+		return loser;
+	}
+	
+	public void simulateRound() {
+		for (int i = 0; i < matches.size(); i++) {
+			Match current = matches.get(i);
+			if (teams.contains(current.getHome()) && teams.contains(current.getAway())) { //Not simulating an already done match
+				current.simulateNeutralField();
+				if (current.getgHome() > current.getgAway()) {
+					teams.remove(current.getAway());
+				}
+				else if (current.getgAway() > current.getgHome()) {
+					teams.remove(current.getHome());
+				}
+				else {
+					teams.remove(simulatePenalties(current));
+				}
+			}
+		}
 	}
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,9 +1,61 @@
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import classes.League;
+import classes.Team;
+
 public class Main {
 	public static void main(String args[])
 	{
-		
+		ArrayList<Team> teams = new ArrayList<Team>();
+		teams.add(new Team("Team A", (byte) 10));
+		teams.add(new Team("Team B", (byte) 10));
+		teams.add(new Team("Team C", (byte) 10));
+		teams.add(new Team("Team D", (byte) 10));
+		teams.add(new Team("Team E", (byte) 10));
+		teams.add(new Team("Team F", (byte) 10));
+		teams.add(new Team("Team G", (byte) 10));
+		teams.add(new Team("Team H", (byte) 10));
+		League testLeague = new League(teams);
+		testLeague.generateCalendar();
+		testLeague.simulateLeague();
+		try {
+			File exportTable = new File("League.csv");
+			if (exportTable.createNewFile()) {
+				FileWriter fw = new FileWriter(exportTable);
+				fw.write("Name,Wins,Draws,Losses,Goals for,Goals against,Points\n");
+				for (byte i = 0; i < teams.size(); i++) {
+					Team current = teams.get(i);
+					fw.write(current.getName() + "," + current.getWins() + "," + current.getDraws() + "," + current.getLosses() +
+							"," + current.getgFor() + "," + current.getgAgainst() + "," + current.getPoints() + "\n");
+				}
+				fw.close();
+				System.out.println("File successfully created at " + exportTable.getAbsolutePath());
+			}
+			else {
+				System.out.println("The csv file already exists and I'm not overwriting it.");
+			}
+			
+			File exportResults = new File("Result_log.txt");
+			if (exportResults.createNewFile()) {
+				FileWriter fw = new FileWriter(exportResults);
+				for (byte i = 0; i < testLeague.getMatches().size(); i++) {
+					fw.write(testLeague.getMatches().get(i) + "\n");
+				}
+				fw.close();
+				System.out.println("File successfully created at " + exportResults.getAbsolutePath());
+			}
+			else {
+				System.out.println("The log file already exists and I'm not overwriting it.");
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
